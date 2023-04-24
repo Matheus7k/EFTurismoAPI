fa6910e0891d5162780820e63e87c01f9c84b916
matheus7k <06matheus.oliveira@gmail.com>
2023-04-24 01:35:13 -0300

    Migrations

---
+++ EFTurismoAPI/Migrations/20230424042906_AlterNumber.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFTurismoAPI.Migrations
+{
+    public partial class AlterNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Numbert",
+                table: "Address",
+                newName: "Number");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Number",
+                table: "Address",
+                newName: "Numbert");
+        }
+    }
+}
